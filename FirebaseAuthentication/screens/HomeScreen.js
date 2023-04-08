4ab2f00f2ac305ccbb9c2e1268c86e905fcd7945
Bhall1234 <ben.hall17@outlook.com>
2023-04-08 12:57:27 +0100

    everything works as intended

--- FirebaseAuthentication/screens/HomeScreen.js
+++ FirebaseAuthentication/screens/HomeScreen.js
@@ -1,12 +1,24 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+
+    const navigation = useNavigation();
+
+    const handleSignOut = () => {
+        auth.signOut().then(() => {
+          navigation.replace("Login")
+            console.log('User signed out!');
+        })
+        .catch(error => alert(error.message));
+    };
+
     return (
         <View style={styles.container}>
           <Text style={styles.title}>Email: {auth.currentUser?.email}</Text>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity onPress={handleSignOut} style={styles.button}>
             <Text style={styles.buttonText}>Logout</Text>
           </TouchableOpacity>
         </View>
