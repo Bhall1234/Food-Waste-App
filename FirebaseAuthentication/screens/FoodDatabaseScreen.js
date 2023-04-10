872a8f272386f88a8fc6dd3595cabd8bc10aa4d8
Bhall1234 <ben.hall17@outlook.com>
2023-04-10 16:04:43 +0100

    Displays the images, when it was taken!

--- FirebaseAuthentication/screens/FoodDatabaseScreen.js
+++ FirebaseAuthentication/screens/FoodDatabaseScreen.js
@@ -1,14 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { collection, query, onSnapshot } from 'firebase/firestore';
+import { firestore } from '../firebase';
 
 const FoodDatabaseScreen = () => {
+  const [foodItems, setFoodItems] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(firestore, 'foodItems'));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({ id: doc.id, ...doc.data() });
+      });
+      setFoodItems(items);
+    });
+
+    // Clean up the listener when the component is unmounted
+    return () => unsubscribe();
+  }, []);
+
   return (
-    <View>
-      <Text>FoodDatabaseScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Food Database</Text>
+      <FlatList
+        data={foodItems}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.category}>Category: {item.category}</Text>
+            <Text style={styles.date}>Date: {item.date.toDate().toDateString()}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default FoodDatabaseScreen
+export default FoodDatabaseScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  itemContainer: {
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: '#61DAFB',
+    borderRadius: 5,
+    padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  category: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  date: {
+    fontSize: 14,
+  },
+});
