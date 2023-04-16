ba02d1636afbe3b5cb7f800688fa505b0d071185
Bhall1234 <ben.hall17@outlook.com>
2023-04-16 22:38:42 +0100

    ammedment to notification

--- FirebaseAuthentication/screens/PhotoScreen.js
+++ FirebaseAuthentication/screens/PhotoScreen.js
@@ -70,7 +70,7 @@ const PhotoScreen = () => {
         'Item Expiring Soon',
         `${title} will expire in ${Math.ceil(daysUntilExpiry)} day(s). Please consume or dispose of it.`,
         {
-          seconds: 0,
+          seconds: secondsToTrigger,
           channelId: 'default',
         }
       );
